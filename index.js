0f024010fc211cd43611bbb1e65e4c87aa9e1770
Matanda Phiri <matandaphiri64@gmail.com>
2024-12-30 04:04:59 -0600

    Write symbol to Grid

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 //IIFE to run game
 const Gameboard = (function(){
     let gameboard = [[".",".","."],[".",".","."],[".",".","."]]
+    const playGrid = Array.from(document.getElementsByTagName('td'));
+
 
     function checkWinner(playerSymbol){
         if(gameboard[0].every === playerSymbol){
@@ -43,19 +45,35 @@ const Gameboard = (function(){
 
     function resetBoard(){
         gameboard = [[".",".","."],[".",".","."],[".",".","."]]
-    }
+        setTimeout(() => {
+            playGrid.forEach(elem => elem.textContent = "")
+        }, 2000);
+        
+    }    
 
-    function play(playerSymbol, row, column){
-        gameboard[row][column] = playerSymbol;
-        console.log(gameboard);
-        if(checkWinner(playerSymbol)){
-            console.log(`${playerSymbol} wins!`);
-            resetBoard();
-        }
-        else if(checkTie()){
-            console.log(`It's a tie!`);
-            resetBoard();
-        }
+    function play(playerSymbol){
+        playGrid.forEach(elem => {
+            elem.addEventListener('click', () => {
+                const row = Number(elem.getAttribute("row"))
+                const col = Number(elem.getAttribute("row"))
+
+                //check if space is empty
+                if(gameboard[row][col] === "."){
+                    gameboard[row][col] = playerSymbol;
+                    elem.textContent = playerSymbol;
+                }   
+                
+                //check for winner or tie
+                if(checkWinner(playerSymbol)){
+                    console.log(`${playerSymbol} wins!`);
+                    resetBoard();
+                }
+                else if(checkTie()){
+                    console.log(`It's a tie!`);
+                    resetBoard();
+                }
+            });
+        });
     }
 
     return {
@@ -78,13 +96,13 @@ const Player = function(sign){
 //Test game
 const player1 = Player("x")
 const player2 = Player("o")
-Gameboard.play(player1.symbol, 0, 0)
-Gameboard.play(player1.symbol, 1, 0)
-Gameboard.play(player2.symbol, 2, 0)
-Gameboard.play(player2.symbol, 0, 1)
-Gameboard.play(player1.symbol, 1, 1)
-Gameboard.play(player2.symbol, 2, 1)
-Gameboard.play(player1.symbol, 0, 2)
-Gameboard.play(player2.symbol, 1, 2)
-Gameboard.play(player2.symbol, 2, 2)
+Gameboard.play(player1.symbol)
+// Gameboard.play(player1.symbol, 1, 0)
+// Gameboard.play(player2.symbol, 2, 0)
+// Gameboard.play(player2.symbol, 0, 1)
+// Gameboard.play(player1.symbol, 1, 1)
+// Gameboard.play(player2.symbol, 2, 1)
+// Gameboard.play(player1.symbol, 0, 2)
+// Gameboard.play(player2.symbol, 1, 2)
+// Gameboard.play(player2.symbol, 2, 2)
 
