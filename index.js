f4d672bc1afe3171f1ba6dcceac8742fce2731af
Matanda Phiri <matandaphiri64@gmail.com>
2024-12-29 19:41:17 -0600

    Add validation to check if player won

--- index.js
+++ index.js
@@ -11,10 +11,29 @@ const Gameboard = (function(){
         else if(gameboard[2].every === playerSymbol){
             return true;
         }
+        else if(gameboard[0][0].every === playerSymbol && gameboard[1][1].every === playerSymbol && gameboard[2][2].every === playerSymbol){
+            return true;
+        }
+        else if(gameboard[0][2].every === playerSymbol && gameboard[1][1].every === playerSymbol && gameboard[2][0].every === playerSymbol){
+            return true;
+        }
+        else if(gameboard[0][0].every === playerSymbol && gameboard[1][0].every === playerSymbol && gameboard[2][0].every === playerSymbol){
+            return true;
+        }
+        else if(gameboard[0][1].every === playerSymbol && gameboard[1][1].every === playerSymbol && gameboard[2][1].every === playerSymbol){
+            return true;
+        }
+        else if(gameboard[0][2].every === playerSymbol && gameboard[1][2].every === playerSymbol && gameboard[2][2].every === playerSymbol){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
     function play(playerSymbol, row, column){
         gameboard[row][column] = playerSymbol
+        
     }
 
     return {
