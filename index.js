2d969ae523efeb71db3395d2962c5b270fb91e82
Matanda Phiri <matandaphiri64@gmail.com>
2024-12-29 19:22:26 -0600

    Validayte rows to check for winner

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ const Gameboard = (function(){
     const gameboard = [[".",".","."],[".",".","."],[".",".","."]]
 
     function checkWinner(playerSymbol){
-        
+        if(gameboard[0].every === playerSymbol){
+            return true;
+        }
+        else if(gameboard[1].every === playerSymbol){
+            return true;
+        }
+        else if(gameboard[2].every === playerSymbol){
+            return true;
+        }
     }
 
     function play(playerSymbol, row, column){
