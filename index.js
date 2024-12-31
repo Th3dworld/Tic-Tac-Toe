331e63455bd5a9e447553eefad32d9fbbbd7a4e5
Matanda Phiri <matandaphiri64@gmail.com>
2024-12-31 11:31:49 -0600

    Finish project

--- index.js
+++ index.js
@@ -2,11 +2,15 @@
 const Gameboard = (function(){
 
     //Declare Variables
-    let gameboard = [[".",".","."],[".",".","."],[".",".","."]]
+    const player1Score = document.querySelector("#contestant-1 > .score");
+    const player2Score = document.querySelector("#contestant-2 > .score");
+    const displayWinner = document.getElementById("display-winner");
     const playGrid = Array.from(document.getElementsByTagName('td'));
+    let gameboard = [[".",".","."],[".",".","."],[".",".","."]]
+    let player1;
     let turn = 0;
     let round = 0;
-
+    
     //Check for winner or tie
     function checkWinner(playerSymbol){
         if(gameboard[0].every(elem => elem === playerSymbol)){
@@ -43,7 +47,7 @@ const Gameboard = (function(){
         state.push(gameboard[0].every(elem => elem !== "."))
         state.push(gameboard[1].every(elem => elem !== "."))
         state.push(gameboard[2].every(elem => elem !== "."))
-
+        
         return state.every(elem => elem === true);
     }
 
@@ -53,53 +57,66 @@ const Gameboard = (function(){
         setTimeout(() => {
             playGrid.forEach(elem => elem.textContent = "")
             gameboard = [[".",".","."],[".",".","."],[".",".","."]]
+            displayWinner.textContent = ``;
         }, 2000);
     }    
 
     function play(player1, player2){
-        let playerSymbol;
+        let player;
+
+        //Initialize scores
+        player1Score.textContent = player1.getPoints();
+        player2Score.textContent = player2.getPoints();
 
         playGrid.forEach(elem => {
             elem.addEventListener('click', () => {
+
+                
 
                 //Determine turns and change sides
                 if(round%2 === 0){
                     console.log("reached here")
                     console.log(turn)
                     if(turn%2 === 0){
-                        playerSymbol = player1.symbol
+                        player = player1
                     }else{
-                        playerSymbol = player2.symbol
+                        player = player2
                     }
                 }else if(round%2 !== 0){
                     console.log("reached")
                     console.log(turn)
                     if(turn%2 === 0){
-                        playerSymbol = player2.symbol
+                        player = player2
                     }else{
-                        playerSymbol = player1.symbol
+                        player = player1
                     }
                 }
 
-                if(!checkWinner(player1.symbol) && !checkWinner(player2.symbol) && !checkTie(playerSymbol)){
+                if(!checkWinner(player1.symbol) && !checkWinner(player2.symbol) && !checkTie(player.symbol)){
                     const row = Number(elem.getAttribute("row"))
                     const col = Number(elem.getAttribute("col"))
 
                     //check if space is empty
-                    if(gameboard[row][col] === "." && !checkWinner(playerSymbol)){
-                        gameboard[row][col] = playerSymbol;
-                        elem.textContent = playerSymbol;
+                    if(gameboard[row][col] === "." && !checkWinner(player.symbol)){
+                        gameboard[row][col] = player.symbol;
+                        elem.textContent = player.symbol;
                         turn++;
 
                         //check for winner or tie
-                        if(checkWinner(playerSymbol)){
-                            console.log(`${playerSymbol} wins!`);
+                        if(checkWinner(player.symbol)){
+                            displayWinner.textContent = `${player.playerName} wins!`;
+                            player.incrementsPoints()
+
                             resetBoard();
                         }
                         else if(checkTie()){
-                            console.log(`It's a tie!`);
+                            displayWinner.textContent = `It's a tie!`;
                             resetBoard();
                         }
+                        
+                        //update scores after each round
+                        player1Score.textContent = player1.getPoints();
+                        player2Score.textContent = player2.getPoints();
                     }  
                 }
             });
@@ -120,7 +137,7 @@ const Player = function(sign, name){
     let points = 0;
     let playerName = name;
 
-    const getPoints = () => {points};
+    const getPoints = () => String(points);
     const incrementsPoints = () => {++points};
     const resetPoints = () => {points = 0};
 
@@ -134,8 +151,14 @@ const player1input = document.querySelector("#player-1")
 const player2input = document.querySelector("#player-2")
 const player1Name = document.querySelector("#contestant-1 > .name");
 const player2Name = document.querySelector("#contestant-2 > .name");
-let player1Score = document.querySelector("#contestant-1 > .score");
-let player2Score = document.querySelector("#contestant-2 > .score");
+const resetBtn = document.getElementById("reset-button")
+let player2;
+
+function runGame(){
+    player1 = Player("x", player1Name.textContent);
+    player2 = Player("o", player2Name.textContent);
+    Gameboard.play(player1, player2);
+}
 
 //Run Modal operations
 dialog.showModal();
@@ -144,10 +167,6 @@ submitBtn.addEventListener("click", (e) =>{
     e.preventDefault();
     player1Name.textContent = player1input.value;
     player2Name.textContent = player2input.value;
-    player1Score.textContent = "0";
-    player2Score.textContent = "0";
-
-
     dialog.close()
 });
 
@@ -155,10 +174,15 @@ dialog.addEventListener("close", ()=>{
     //Reset the values
     player1input.value = ""
     player2input.value = ""
+    //Run the game
+    runGame();
 });
 
-//Run the game
-const player1 = Player("x", player1Name);
-const player2 = Player("o", player2Name);
-Gameboard.play(player1, player2);
+resetBtn.addEventListener("click", ()=>{
+    dialog.showModal();
+});
+
+console.log("yes")
+
+
 
