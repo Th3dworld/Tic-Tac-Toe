c6db9017281d2c4ed7e4a219add53ba227a8fb80
Matanda Phiri <matandaphiri64@gmail.com>
2025-01-01 04:36:00 -0600

    Update to show who's turn it is and to reset points

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const Gameboard = (function(){
     const displayWinner = document.getElementById("display-winner");
     const playGrid = Array.from(document.getElementsByTagName('td'));
     let gameboard = [[".",".","."],[".",".","."],[".",".","."]]
-    let player1;
     let turn = 0;
     let round = 0;
     
@@ -59,16 +58,34 @@ const Gameboard = (function(){
             gameboard = [[".",".","."],[".",".","."],[".",".","."]]
             displayWinner.textContent = ``;
         }, 2000);
-    }    
+    }
+    
+    function instaReset(){
+        playGrid.forEach(elem => elem.textContent = "")
+        gameboard = [[".",".","."],[".",".","."],[".",".","."]]
+        displayWinner.textContent = ``;
+        turn = 0;
+        round = 0;
+    }
 
     function play(player1, player2){
         let player;
+        console.log(player1)
+        console.log(player2)
 
-        //Initialize scores
+        //reset the board
+        instaReset();
+
+        //Initialize scores and turn
         player1.resetPoints();
         player2.resetPoints();
         player1Score.textContent = player1.getPoints();
         player2Score.textContent = player2.getPoints();
+        displayWinner.textContent = `${player1.playerName}'s turn`;
+        
+        console.log(player1)
+        console.log(player2)
+        
 
         playGrid.forEach(elem => {
             elem.addEventListener('click', () => {
@@ -77,16 +94,12 @@ const Gameboard = (function(){
 
                 //Determine turns and change sides
                 if(round%2 === 0){
-                    console.log("reached here")
-                    console.log(turn)
                     if(turn%2 === 0){
                         player = player1
                     }else{
                         player = player2
                     }
                 }else if(round%2 !== 0){
-                    console.log("reached")
-                    console.log(turn)
                     if(turn%2 === 0){
                         player = player2
                     }else{
@@ -107,7 +120,8 @@ const Gameboard = (function(){
                         //check for winner or tie
                         if(checkWinner(player.symbol)){
                             displayWinner.textContent = `${player.playerName} wins!`;
-                            player.incrementsPoints()
+                            console.log(player.playerName);
+                            player.incrementsPoints();
 
                             resetBoard();
                         }
@@ -115,10 +129,19 @@ const Gameboard = (function(){
                             displayWinner.textContent = `It's a tie!`;
                             resetBoard();
                         }
+                        else{//Update turn each round
+                            if(player.playerName === player1.playerName){
+                                displayWinner.textContent = `${player2.playerName}'s turn`;
+                            }else{
+                                displayWinner.textContent = `${player1.playerName}'s turn`;
+                            }
+                        }
                         
                         //update scores after each round
                         player1Score.textContent = player1.getPoints();
                         player2Score.textContent = player2.getPoints();
+
+                        
                     }  
                 }
             });
@@ -126,10 +149,7 @@ const Gameboard = (function(){
     }
 
     return {
-        gameboard,
-        play,
-        checkTie,
-        checkWinner
+        play
     };
 })();
 
@@ -154,11 +174,15 @@ const player2input = document.querySelector("#player-2")
 const player1Name = document.querySelector("#contestant-1 > .name");
 const player2Name = document.querySelector("#contestant-2 > .name");
 const resetBtn = document.getElementById("reset-button")
+let player1;
 let player2;
+
 
 function runGame(){
     player1 = Player("x", player1Name.textContent);
     player2 = Player("o", player2Name.textContent);
+    console.log(player1)
+    console.log(player2)
     Gameboard.play(player1, player2);
 }
 
@@ -169,6 +193,8 @@ submitBtn.addEventListener("click", (e) =>{
     e.preventDefault();
     player1Name.textContent = player1input.value;
     player2Name.textContent = player2input.value;
+    console.log(player1Name.textContent)
+    console.log(player2Name.textContent)
     dialog.close()
 });
 
